31c750f4dcd7afa3a19671fb9396f8d999e7e605
Mark Fisher <mark.j.fisher@gmail.com>
2023-10-18 19:49:05 +0100

    Trying to get http delete working and returning data

--- httpbin/src/httpbin.c
+++ httpbin/src/httpbin.c
@@ -11,11 +11,88 @@
 #endif
 
 #include "debug.h"
+#include "httpbin.h"
 
-char *httpbin = "N:HTTP://httpbin.org/";
+char *httpbin = "N:HTTPS://httpbin.org/";
+char *del_path = "N:HTTP://192.168.1.85:5987/delete";
 char url_buffer[128];
 char result[1024];
 uint8_t err = 0;
+char *url;
+uint16_t conn_bw;
+uint8_t connected;
+uint8_t conn_err;
+
+char *version = "v1.2.0";
+
+void main(void) {
+    setup();
+
+    printf("httpbin %s\n", version);
+    printf("Base URL: %s\n", httpbin);
+
+    test_post();
+    test_put();
+    test_delete();
+
+    exit(0);
+}
+
+void setup() {
+    bzero(url_buffer, 128);
+    bzero(result, 1024);
+    gotox(0);
+}
+
+// -------------------------------------------------------------------------------
+// POST JSON, get result
+void test_post() {
+    url = create_url("post");
+    err = network_open(url, OPEN_MODE_HTTP_POST, OPEN_TRANS_NONE);
+    handle_err("post:open");
+
+    post(url, "{\"name\":\"fenrock\"}");
+    err = network_json_parse(url);
+    handle_err("post:json parse");
+    network_json_query(url, "N:/json/name", result);
+    handle_err("post:json query");
+    printf("/post   :  name=%s\n", result);
+    network_close(url);
+    handle_err("post:close");
+}
+
+// -------------------------------------------------------------------------------
+// PUT JSON, get result. PUT is just a slightly different POST
+void test_put() {
+    url = create_url("put");
+    err = network_open(url, OPEN_MODE_HTTP_PUT_H, OPEN_TRANS_NONE);
+    handle_err("put:open");
+
+    post(url, "{\"level\":11}");
+    err = network_json_parse(url);
+    handle_err("put:json parse");
+    network_json_query(url, "N:/json/level", result);
+    handle_err("put:json query");
+    printf("/put    : level=%s\n", result);
+    network_close(url);
+    handle_err("put:close");
+}
+
+// -------------------------------------------------------------------------------
+// DELETE - no data to send, but response will have data in it
+void test_delete() {
+    // url = del_path;
+    url = create_url("delete");
+    del(url);
+    // err = network_json_parse(url);
+    printf("/delete :    .%s,\n", result);
+    // handle_err("delete:json parse");
+    // network_json_query(url, "N:/url", result);
+    // handle_err("delete:json query");
+    // printf("/delete :   url=%s\n", result);
+    network_close(url);
+    handle_err("delete:close");
+}
 
 void handle_err(char *reason) {
     if (err) {
@@ -29,17 +106,17 @@ char *create_url(char *method) {
     return (char *) url_buffer;
 }
 
-uint8_t set_http_channel_mode(char *devicespec, uint8_t mode) {
 // This demonstrates why library should have specialised functions, instead of consumers having to use ifdef
-#ifdef BUILD_APPLE2
+uint8_t set_http_channel_mode(char *devicespec, uint8_t mode) {
+    #ifdef BUILD_APPLE2
     // uint8_t network_ioctl(uint8_t cmd, uint8_t aux1, uint8_t aux2, char* devicespec, int16_t use_aux, void *buffer, uint16_t len);
     return network_ioctl('M', 0, mode, devicespec, 1, 0, 2);
-#endif
+    #endif
 
-#ifdef BUILD_ATARI
+    #ifdef BUILD_ATARI
     // uint8_t network_ioctl(uint8_t cmd, uint8_t aux1, uint8_t aux2, char* devicespec, [uint8_t dstats, uint16_t dbuf, uint16_t dbyt]);
     return network_ioctl('M', 0, mode, devicespec, 0, 0, 0);
-#endif
+    #endif
 }
 
 void add_header(char *devicespec, char *header) {
@@ -50,12 +127,37 @@ void add_header(char *devicespec, char *header) {
 }
 
 void post(char *devicespec, char *data) {
+    set_json(url);
     err = set_http_channel_mode(devicespec, HTTP_CHAN_MODE_POST_SET_DATA);
     handle_err("post chan mode");
     err = network_write(devicespec, (uint8_t *) data, strlen(data));
     handle_err("post: write");
     err = set_http_channel_mode(devicespec, HTTP_CHAN_MODE_BODY);
     handle_err("body chan mode");
+}
+
+void del(char *devicespec) {
+    printf("deleting %s\n", devicespec);
+    #ifdef BUILD_APPLE2
+    // uint8_t network_ioctl(uint8_t cmd, uint8_t aux1, uint8_t aux2, char* devicespec, int16_t use_aux, void *buffer, uint16_t len);
+    err = network_ioctl('!', 0, 0, devicespec, 1, 0, 0);
+    #endif
+
+    debug();
+    #ifdef BUILD_ATARI
+    // uint8_t network_ioctl(uint8_t cmd, uint8_t aux1, uint8_t aux2, char* devicespec, uint8_t dstats, uint16_t dbuf, uint16_t dbyt);
+    // dbuf is used for the data sent to FN to say what the URL path is.
+    // devicespec is same, but is used for the device id. it's ioctl, we forgive it for being a tool for all
+    // dstats has to be 0x80 to send, else FN crashes(!)
+    err = network_ioctl('!', 0, 0, devicespec, 0x80, devicespec, 256);
+    #endif
+    handle_err("del: ioctl");
+    printf("checking status\n");
+    err = network_status(devicespec, &conn_bw, &connected, &conn_err);
+    handle_err("del: status");
+    printf("BW: %d, c: %d, e: %d\n", conn_bw, connected, conn_err);
+    err = network_read(devicespec, (uint8_t *) result, conn_bw);
+    handle_err("body read");
 }
 
 void body(char *devicespec, char *r, uint16_t len) {
@@ -69,31 +171,4 @@ void body(char *devicespec, char *r, uint16_t len) {
 void set_json(char *devicespec) {
     add_header(devicespec, "Accept: application/json");
     add_header(devicespec, "Content-Type: application/json");
-}
-
-void main(void) {
-    char *url;
-    bzero(url_buffer, 128);
-    bzero(result, 1024);
-
-    gotox(0);
-    printf("httpbin v1.0.6\n\n");
-
-    // POST JSON, get result
-    url = create_url("post");
-    printf("Post to: [%s]\n", url);
-    err = network_open(url, OPEN_MODE_HTTP_POST, OPEN_TRANS_NONE);
-    handle_err("post:open");
-
-    set_json(url);
-    post(url, "{\"name\":\"fenrock\"}");
-    err = network_json_parse(url);
-    handle_err("post:json parse");
-    network_json_query(url, "N:/json/name", result);
-    handle_err("post:json query");
-    printf("POST: name = %s\n", result);
-    network_close(url);
-    handle_err("post:close");
-
-    exit(0);
 }
---
+++ httpbin/src/httpbin.h
@@ -0,0 +1,21 @@
+#ifndef HTTPBIN_H
+#define HTTPBIN_H
+
+#include <stdint.h>
+
+void add_header(char *devicespec, char *header);
+char *create_url(char *method);
+void handle_err(char *reason);
+uint8_t set_http_channel_mode(char *devicespec, uint8_t mode);
+void set_json(char *devicespec);
+void setup();
+
+void body(char *devicespec, char *r, uint16_t len);
+void del(char *devicespec);
+void post(char *devicespec, char *data);
+
+void test_delete();
+void test_post();
+void test_put();
+
+#endif
